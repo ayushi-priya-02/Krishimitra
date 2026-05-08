7fc0cdfc64e5679993a42280da8db5e3fab3976a
ayushi-priya-02 <ayushinwd01@gmail.com>
2026-05-08 19:58:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
@@ -71,6 +72,6 @@ def home():
         water=water
     )
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
